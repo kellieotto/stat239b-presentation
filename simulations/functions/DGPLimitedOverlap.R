80518d32886739e305f06553e01ab8c651bd26bc
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-04-04 09:50:39 -0700

    Add DGP where covariate distributions of tr and ctrl vary

---
+++ simulations/functions/DGPLimitedOverlap.R
@@ -0,0 +1,27 @@
+# Data generating process with limited covariate overlap
+# We keep assumptions 3.2-3.4 from Abadie-Imbens
+# Change the covariate distribution: for mu > 0,
+# X ~ N(0,1) for the treated group
+# X ~ N(mu,1) for the control group
+# Limited covariate overlap will mean that few controls, with small X values, will be matched many times.
+# In bootstrap samples where these small controls don't appear, matches will be poor.
+# In bootstrap samples where they do appear, their weight K will much larger than in the original sample.
+
+DGPLimitedOverlap <- function(n.sam, tau, alpha, mu) {
+  
+  # Assumption 3.2/3.3
+  n0 <- round(n.sam / (1 + alpha))
+  W <- rep(1, n.sam)
+  W0 <- sample(1:n.sam, n0)
+  W[W0] <- 0
+  
+  # Assumption 3.4
+  Y <- rep(tau, n.sam)
+  Y[W0] <- rnorm(n0)
+  
+  # Generate X
+  X <- rnorm(n.sam)
+  X[W0] <- X[W0]+3
+  
+  return(data.frame(Y = Y, W = W, X = X))
+}
---
+++ simulations/sims/LimitedOverlapDGP.R
@@ -0,0 +1,90 @@
+library(parallel)
+library(rlecuyer)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+
+
+system.time({
+  
+# Load the functions
+setwd("../functions/")
+invisible(sapply(list.files(), source))
+setwd("../sims/")
+
+# parameters to tweak
+n.sam <- 1000
+alpha <- 2
+tau <- 5
+n.sim <- 10
+mu <- seq(0, 5, by = 0.5)
+
+# Parallel Backend Setup
+RNGkind("L'Ecuyer-CMRG") # So we actually have random data
+n.cores <- detectCores() - 2
+cl <- makeCluster(n.cores, type="SOCK")
+clusterExport(cl=cl, 
+              varlist = c("BootFindMatches", 
+                          "BootMatchedATE", 
+                          "Bootstrap", 
+                          "CalcBootstrapVar", 
+                          "CalcMatchedATE", 
+                          "DGPLimitedOverlap", 
+                          "FindMatches", 
+                          "SimulateDGP",
+                          "nn2", # nn2 from RANN package
+                          "n.sam",
+                          "alpha",
+                          "tau",
+                          "mu"
+              )
+)
+
+
+# Simulation Call
+simulation <- lapply(mu, function(mm){
+                          parReplicate(cl, n.sim, 
+                           SimulateDGP(DGPLimitedOverlap, n.sam = n.sam, alpha = alpha, tau = tau, mu = mm))
+              })
+
+# Free up resources on your computer again
+save(simulation, file = "LimitedOverlapDGP_Results.RData")
+stopCluster(cl)
+
+})
+
+##################################################################################################################
+###### Make simulation results usable
+
+res <- data.frame(do.call(rbind, simulation))
+res <- res %>% mutate(mu = rep(mu, each = n.sim))
+
+###### RMSE of the bootstrap variances
+
+# suppose that the variance of the n.sim tau_hats is the "true" variance
+# take RMSE relative to that.
+
+res2 <- res %>% group_by(mu) %>%
+  summarise(tau.hat.overall = mean(tau.hat),
+            tau.hat.var.obs = var(tau.hat),
+            tau.boot.overall = mean(tau.boot),
+            tau.boot.var.obs = var(tau.boot),
+            var.boot.overall = mean(var.boot),
+            var.boot.var     = var(var.boot),
+            var.boot.median  = median(var.boot),
+            var.boot.rmse    = sqrt(mean((var.boot - var(tau.hat))^2))
+  )
+
+res2_melt <- melt(res2, id.vars = "mu")
+res2_melt %>% filter(variable %in% c("var.boot.overall", "tau.hat.var.obs")) %>%
+  ggplot(aes(x = mu, y = value, color = variable)) + geom_line()
+
+res_melt <- melt(res, id.vars = "mu")
+res_melt %>% filter(variable == "var.boot") %>%
+  ggplot(aes(x = factor(mu), y = value)) +
+  geom_boxplot() +
+  geom_hline(yintercept = mean(res2$tau.hat.var.obs), linetype = "dashed") + # Empirical estimate of "true" variance of tau_hat. Since it doesn't depend on mu, we average over all 11*1000 simulations.
+  xlab("mu") +
+  ylab("Bootstrap Variance") +
+  ggtitle("Bootstrap Variance across 1000 Simulations")
+

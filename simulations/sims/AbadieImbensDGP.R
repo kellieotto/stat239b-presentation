7fad234ef4334a9b899e78a76863bdbb2dec58a2
Andrew Do <do@berkeley.edu>
2016-04-07 06:49:11 -0700

    clean job

--- simulations/sims/AbadieImbensDGP.R
+++ simulations/sims/AbadieImbensDGP.R
@@ -1,7 +1,10 @@
 library(parallel)
 library(rlecuyer)
+library(plyr)
 
 # Load the functions
+wd <- Sys.getenv("R_WD")
+setwd(wd)
 path.to.functions <- "../functions/"
 functions <- list.files(path.to.functions)
 invisible(sapply(paste0(path.to.functions, functions), source))
@@ -14,7 +17,9 @@ n.sim <- 1000
 
 # Parallel Backend Setup
 RNGkind("L'Ecuyer-CMRG") # So we actually have random data
-n.cores <- detectCores() - 2
+n.cores <- detectCores()
+core.message <- sprintf("Using %i cores", n.cores)
+print(core.message)
 cl <- makeCluster(n.cores, type="SOCK")
 clusterExport(cl=cl, 
               varlist = c("BootFindMatches", 
@@ -26,8 +31,6 @@ clusterExport(cl=cl,
                           "FindMatches", 
                           "SimulateDGP",
                           "nn2", # nn2 from RANN package
-                          "n.sam",
-                          "alpha",
                           "tau"
                           )
               )
@@ -35,11 +38,12 @@ clusterExport(cl=cl,
 
 # Simulation Call
 
-simulation <- apply(expand.grid(n.sam, alpha), 1,
+simulation <- alply(expand.grid(n = n.sam, a = alpha), 1,
   function(x) {
     env <- environment()
-    n <- x[1]; a <- x[2]
+    n <- x$n; a <- x$a
     clusterExport(cl = cl, varlist = c("n", "a"), envir = env)
+    print(sprintf("Now working on n = %i and a = %i", n, a))
     parReplicate(cl, n.sim, SimulateDGP(DGPAbadieImbens, 
                                         n.sam = n, 
                                         alpha = a, 
@@ -47,11 +51,14 @@ simulation <- apply(expand.grid(n.sam, alpha), 1,
     }
 )
 
+# Save Results
+save(simulation, file = "AbadieImbensDGP_Results.RData")
+
 # Free up resources on your computer again
 stopCluster(cl)
 
 # <--- Verifying Lemma 3.1 --->#
-
+if (FALSE) {
 var(simulation[ ,"tau.hat"])
 n0 <- round(n.sam/(1+alpha))
 n1 <- n.sam - n0
@@ -61,3 +68,4 @@ exact.var.tau.hat <- 1/n1 + 3/2 * (n1 -1) * (n0 + 8/3) / n1 / (n0 + 1) / (n0 + 2
 scaled <- sqrt(n1) * (simulation[ ,"tau.hat"] - tau)
 mean(scaled)
 var(scaled)
+}
---
+++ simulations/sims/job.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export R_WD="~/Documents/stat239b-presentation/simulations/sims/"
+
+R CMD BATCH AbadieImbensDGP.R
